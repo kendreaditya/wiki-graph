0ed8a5cae51f26ebeba20368dabe778e36e94ee2
Aditya Kendre <kendreaditya@gmail.com>
2021-05-23 14:54:37 -0400

    Added split node/edge and link rendering     - created renderGraphNodes and renderGraphLinks     - removed links and nodes state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {InteractiveForceGraph, ForceGraphNode, ForceGraphLink, set} from 'react-vis-force';
+import {InteractiveForceGraph, ForceGraphNode, ForceGraphLink} from 'react-vis-force';
 import Graph from './components/graph';
 import {getLinks} from './components/api'
 import Search from "./components/search";
+import { render } from '@testing-library/react';
 
 // render as more responces come in
 // add ability to add and remove nodes -> search list materail ui list
@@ -11,8 +12,27 @@ import Search from "./components/search";
 function App() {
   const [value, setValue] = React.useState(null)
   const [renderedGraph, setRenderedGraph] = React.useState(null);
-  const [nodes, setNodes] = React.useState(new Map()); 
-  const [links, setLinks] = React.useState([]); 
+
+  const renderGraphNodes = (nodes) => {
+    let graphNodes = Array.from(nodes).map(([title, edge])=>{
+      return <ForceGraphNode node={{id: title}}/>
+    })
+    console.log(graphNodes);
+    renderedGraph ? setRenderedGraph([...renderedGraph, ...graphNodes]) : setRenderedGraph([...graphNodes]);  
+  };
+
+  const renderGraphLinks = (links) => {
+    let graphLinks = links.map((link)=>{
+      // fix by checking .length===0
+      console.log(link[0], link[1])
+      return <ForceGraphLink link={{source: link[0], target: link[1]}}/>
+    })
+    renderedGraph ? setRenderedGraph([...renderedGraph, ...graphLinks]) : setRenderedGraph([...graphLinks]);  
+  };
+
+// await setTimeout(function(){
+//   setRenderedGraph(graph)
+// }, 3000);
 
   const getAllLinks = async (rootPage, depth) => {
     const _links = async (page) => {
@@ -30,51 +50,29 @@ function App() {
     };
 
     let pages = [rootPage];
+    setRenderedGraph([<ForceGraphNode node={{ id: value.title}} fill="red" />]);
 
     for(var i=0; i<depth; i++){
       var depthPages = [];
-
+      
       await pages.forEach(async (page)=>{
         let [pageGraphNodes, pageGraphLinks] = await _links(page);
+
+        renderGraphNodes(pageGraphNodes);
+        renderGraphLinks(pageGraphLinks);
+
         depthPages = await [...depthPages, ...pageGraphNodes.values()];
         // Automatically diregards 2nd instance of hash map
-        console.log(pageGraphNodes)
-        setNodes(await new Map([...nodes, ...pageGraphNodes]));
-        setLinks(await [links, ...pageGraphLinks]);
-        console.log(nodes)
-        console.log(links)
       })
 
-      console.log(await depthPages)
       pages = await depthPages;
     }
   }
 
-  const updateGraph = async () => {
-    // Divide links and nodes + don't rerender everything everything (figure out whats newly added)
-    let graph = []
-  
-    graph.push(<ForceGraphNode node={{ id: value.title}} fill="red" />)
 
-    nodes.forEach(async (edge) => {
-      graph.push(<ForceGraphNode node={{id: edge.title}}/>)
-    })
-  
-    links.forEach((link) => {
-      // fix by checking .length===0
-      if(link[0])
-        graph.push(<ForceGraphLink link={{source: link[0], target: link[1]}}/>)
-    })
+  React.useEffect(() => (value ? getAllLinks(value, 1) : null), [value]);
+  React.useEffect(()=> console.log(renderedGraph), [renderedGraph]);
 
-    await setTimeout(function(){
-      setRenderedGraph(graph)
-    }, 3000);
-  }  
-
-
-  React.useEffect(() => (value ? getAllLinks(value, 2) : null), [value])
-
-  React.useEffect(()=> ( value ? updateGraph() : null), [links, nodes])
 
 
   return (
